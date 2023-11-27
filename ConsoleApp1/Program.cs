489ae8f0f868b03ca5dc5471a11b2738f64cc276
Ahmet Ündemir <ahmetundemir@outlook.com>
2023-11-27 13:35:50 +0300

    switch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,19 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
 
+            var day = 1;
+
+            string dayName = day switch
+            {
+                1 => "Monday",
+                2 => "Tuesday",
+                3 => "Wednesday",
+                4 => "Thurday",
+                5 => "Friday",
+                6 => "Saturday",
+                7 => "Sunday",
+                _ => "Invalid Day"
+            };
         }
     }
 }
